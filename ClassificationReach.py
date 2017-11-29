e798eb6ff28a53a54b0dac7dade57ea05c8f138f
banderson1618 <banderson1618@gmail.com>
2017-11-29 11:16:44 -0700

    Added some documentation

--- ClassificationReach.py
+++ ClassificationReach.py
@@ -2,6 +2,7 @@ class ClassificationReach(object):
     def __init__(self, polyline, classification):
         """
         :type polyline: The complete polyline
+        :type classification: What the hydrologic regime of the stream is
         """
         self.polyline = polyline
         self.classification = classification
