f772163a17254760d4177315ff5312d5f2dfad8f
Tyler Hatch <A02079569@aggies.usu.edu>
2020-08-26 17:37:59 -0500

    Now runs much, much quicker

--- HydrologicRegime.py
+++ HydrologicRegime.py
@@ -50,71 +50,60 @@ def makeReaches(streamNetwork, dem, marchPrecip, janTemp, aprilTemp, minWinterTe
     fileOne = open(tempData + "\marchPrecip.txt", 'w')
     fileTwo = open(tempData + "\marchPrecipGreater.txt", 'w')
 
-    polylineCursor = arcpy.da.SearchCursor(streamNetwork, ['SHAPE@'])
-    if testing:
-        for i in range(10):
-            arcpy.AddMessage("Creating Reach " + str(i+1) + " out of 10")
-            row = polylineCursor.next()
-            classification = findClassification(row[0].firstPoint, dem, marchPrecip, janTemp, aprilTemp, minWinterTemp, tempData, sr, fileOne, fileTwo, testing)
+    centroid_save = os.path.join(tempData, "Centroid.shp")
+    centroid_points = arcpy.FeatureToPoint_management(streamNetwork, centroid_save, "INSIDE")
 
-            reach = ClassificationReach(row[0], classification)
-            reaches.append(reach)
-    else:
-        i = 0 # just used for displaying how far through the program it is
-        for row in polylineCursor:
-            i += 1
-            if i%100 == 0:
-                arcpy.AddMessage("Creating Reach " + str(i) + " out of " + numReachesString
-                             + " (" + str((float(i)/float(numReaches))*100) + "% complete)")
-            classification = findClassification(row[0].firstPoint, dem, marchPrecip, janTemp, aprilTemp, minWinterTemp, tempData, sr, fileOne, fileTwo, testing)
-            reach = ClassificationReach(row[0], classification)
-            reaches.append(reach)
+    extract_string = "{} march;{} jan;{} april;{} winter;{} elev"\
+        .format(marchPrecip, janTemp, aprilTemp, minWinterTemp, dem)
 
-    fileOne.close()
-    fileTwo.close()
+    arcpy.gp.ExtractMultiValuesToPoints_sa(centroid_points, extract_string, "BILINEAR")
+    fields_list = ["march", "jan", "april", "winter", "elev"]
+
+    point_cursor = arcpy.da.SearchCursor(centroid_points, fields_list)
+    line_cursor = arcpy.da.SearchCursor(streamNetwork, "SHAPE@")
+
+    # if testing:
+    #     for i in range(10):
+    #         arcpy.AddMessage("Creating Reach " + str(i+1) + " out of 10")
+    #         row = polylineCursor.next()
+    #         classification = findClassification(row[0].firstPoint, dem, marchPrecip, janTemp, aprilTemp, minWinterTemp, tempData, sr, fileOne, fileTwo, testing)
+    #         reach = ClassificationReach(row[0], classification)
+    #         reaches.append(reach)
+
+    i = 0# just used for displaying how far through the program it is
+    for row, line in zip(point_cursor, line_cursor):
+        i += 1
+        if i%10000 == 0:
+            arcpy.AddMessage("Creating Reach " + str(i) + " out of " + numReachesString
+                         + " (" + str((float(i)/float(numReaches))*100) + "% complete)")
+        classification = findClassification(row[0], row[1], row[2], row[3], row[4])
+        reach = ClassificationReach(line[0], classification)
+        reaches.append(reach)
 
     del row
-    del polylineCursor
+    del point_cursor
 
     arcpy.AddMessage("Reach Array Created")
 
     return reaches
 
 
-def findClassification(point, dem, marchPrecip, janTemp, aprilTemp, minWinterTemp, tempData, sr, txtFile, txtFileTwo, testing):
-    arcpy.env.workspace = tempData
-    pointFile = arcpy.CreateFeatureclass_management(tempData, "point.shp", "POINT", "", "DISABLED", "DISABLED", sr)
-    cursor = arcpy.da.InsertCursor(pointFile, ["SHAPE@"])
-    cursor.insertRow([point])
-    del cursor
+def findClassification(march_precip, jan_temp, april_temp, winter_temp, elevation):
 
-    if testing:
-        arcpy.AddMessage("March Precip: " + str(findRasterValueAtPoint(pointFile, marchPrecip, tempData)))
-        arcpy.AddMessage("Elevation: " + str(findRasterValueAtPoint(pointFile, dem, tempData)))
-        arcpy.AddMessage("Min Winter Temp: " + str(findRasterValueAtPoint(pointFile, minWinterTemp, tempData)))
-        arcpy.AddMessage("Jan Temp: " + str(findRasterValueAtPoint(pointFile, janTemp, tempData)))
-        arcpy.AddMessage("________________________________________________________________________")
-
-    marchPrecipNum = findRasterValueAtPoint(pointFile, marchPrecip, tempData)
-
-    txtFile.write(str(marchPrecipNum) + "\n")
-    if marchPrecipNum >= 261.7:
-        txtFileTwo.write(str(marchPrecipNum) + "\n")
-
-    if marchPrecipNum >= 261.7:
-        if findRasterValueAtPoint(pointFile, dem, tempData) < 618: # Finds elevation, branches
+    if march_precip >= 261.7:
+        if elevation < 618: # Finds elevation, branches
             return "Rainfall"
         else:
             return "Rain-Snow"
     else:
-        if marchPrecipNum < 185.6:
-            if findRasterValueAtPoint(pointFile, janTemp, tempData) >= -5: # Finds temp in January, branches based on that
-                if findRasterValueAtPoint(pointFile, aprilTemp, tempData) < 6.26: # Finds temp in April, branch based on that
+        if march_precip < 185.6:
+            if jan_temp >= -5: # Finds temp in January, branches based on that
+                if april_temp < 6.26: # Finds temp in April, branch based on that
                     return "Groundwater"
                 else:
                     return "Snow-Rain"
             else:
-                if findRasterValueAtPoint(pointFile, minWinterTemp, tempData) < -7.7: # Finds winter temperature, branches based on that
+                if winter_temp < -7.7: # Finds winter temperature, branches based on that
                     return "Ultra-Snowmelt"
                 else:
                     return "Snowmelt"
